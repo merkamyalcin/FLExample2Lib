d54a976b4e967c8bfd9b63a365393935470452aa
meyalcin <erkam.yalcin@gmail.com>
2020-08-06 23:08:09 +0300

    first commit2

---
+++ Example/Pods/Target Support Files/FLExample2Lib/FLExample2Lib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FLExample2Lib : NSObject
+@end
+@implementation PodsDummy_FLExample2Lib
+@end
---
+++ Example/Pods/Target Support Files/Pods-FLExample2Lib_Tests/Pods-FLExample2Lib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FLExample2Lib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FLExample2Lib_Tests
+@end
